eebe9adf3193648220f3b82155b6f48895f4184f
Konstantinos Mavrikas <53100825+KMavr@users.noreply.github.com>
2024-11-01 20:22:34 +0200

    Initial setup (#1)
    * Initial Vite setup.
    
    * Setup TailwindCSS and small cleanup.
    
    * Setup prettier.
    
    * Husky and lint-staged setup.
    
    * Setup Mantine UI.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+  return (
+    <MantineProvider>
+      <div>Hello World!</div>
+    </MantineProvider>
+  );
+}
+
+export default App;
